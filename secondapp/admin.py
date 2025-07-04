6237a373f0bc350036c8b183b3225d8d40460867
lukaklanjsek <luka.klanjsek@gmail.com>
2025-07-04 22:17:00 +0200

    technically only experimentations happening on a smaller examples

--- secondapp/admin.py
+++ secondapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import timezone
 
 # Register your models here.
 
@@ -6,22 +7,63 @@ from .models import Attendee
 from .models import Song
 from .models import CurrentRehearsal
 from .models import RehearsalDate
+from .models import AttendanceRecord
 
 # classes
-class AttendeeAdmin(admin.StackedInline):
-    #model = Attendee
-    #fields = ["first_name", "last_name", "instrument"]
-    pass
+class AttendeeAdmin(admin.ModelAdmin):
+    model = Attendee
+    fieldsets = [
+        ("Basic info", {"fields": ["first_name"]}),
+        (None, {"fields": ["last_name"]}),
+        (None, {"fields": ["instrument"]}),
+        (None, {"fields": ["is_active"]}),
+        (None, {"fields": ["skill_level"]}),
+        ("Bio", {"fields": ["phone_number"]}),
+        (None, {"fields":["email"]}),
+        (None, {"fields": ["address"]}),
+        (None, {"fields": ["date_of_birth"]}),
+        (None, {"fields": ["messenger"]}),
+        ("Other", {"fields": ["shirt_size"]}),
+        (None, {"fields": ["date_of_joining"]}),
+        (None, {"fields": ["additional_attendee_notes"]})
+    ]
+    #pass
+
+class AttendanceRecordAdmin(admin.ModelAdmin):
+    list_display = ("attendee", "attendance")
+    list_filter = ("attendance",)
+    search_fields = ("attendee__first_name", "attendee__last_name")
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(attendee__is_active=True)
 
 
 class SongAdmin(admin.ModelAdmin):
+
     pass
 
 class RehearsalDateAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = [
+        (None, {"fields": ["rehearsal_calendar"]}),
+        (None, {"fields": ["rehearsal_subtitle"]}),
+        ("Location", {"fields": ["rehearsal_location"]}),
+        (None, {"fields": ["rehearsal_location_parking"]}),
+    ]
+
+    def save_model(self, request, obj, form, change):
+        timezone.activate(timezone.get_current_timezone())
+        super().save_model(request, obj, form, change)
+
+
+    #pass
 
 class CurrentRehearsalAdmin(admin.ModelAdmin):
-    #list_display = [""]
+    #list_display = ("rehearsal", "song",) #"attendee", "attendance"
+    #list_editable = ("attendance",)
+    #list_filter = ("rehearsal", "song",) #"attendee", "attendance",
+    #search_fields = ("rehearsal__rehearsal_subtitle", "song__song_title", "attendee__first_name", "attendee__last_name",)
+
     #fieldsets = [
     #    (None, {"fields": ["rehearsal_subtitle"]}),
     #    ("Date information", {"fields": ["date_of_rehearsal"], "classes": ["collapse"]}),
@@ -35,7 +77,8 @@ class CurrentRehearsalAdmin(admin.ModelAdmin):
 
 
 
-#admin.site.register(Attendee, AttendeeAdmin)
+admin.site.register(Attendee, AttendeeAdmin)
+admin.site.register(AttendanceRecord, AttendanceRecordAdmin)
 admin.site.register(Song, SongAdmin)
 admin.site.register(CurrentRehearsal, CurrentRehearsalAdmin)
 admin.site.register(RehearsalDate, RehearsalDateAdmin)
--- secondapp/models.py
+++ secondapp/models.py
@@ -1,4 +1,7 @@
+from random import choices
+
 from django.db import models
+import datetime
 from datetime import date
 from enum import Enum
 from django.utils import timezone
@@ -17,18 +20,18 @@ class ShirtSize(Enum):
     XXXL = '3x Extra Large'
 
 
-class VoiceType(Enum):
-    S1 = 'Soprano'
-    MS = 'Mezzo-Soprano'
-    A1 = 'Higher-Alto'
-    A2 = 'Lower-Alto'
-    CA = 'Contra-Alto'
-    CT = 'Counter-Tenor'
-    T1 = 'Higher-Tenor'
-    T2 = 'Lower-Tenor'
-    B1 = 'Baritone'
-    B2 = 'Bass'
-    CB = 'Contra-Bass'
+class VoiceType(models.TextChoices):
+    SOPRANO1 = 'Soprano', 'Soprano'
+    SOPRANO2 = 'Mezzo-Soprano', 'Mezzo-Soprano'
+    ALTO1 = 'Higher-Alto', 'Higher-Alto'
+    ALTO2 = 'Lower-Alto', 'Lower-Alto'
+    CONTRAALTO = 'Contra-Alto', 'Contra-Alto'
+    COUNTERTENOR = 'Counter-Tenor', 'Counter-Tenor'
+    TENOR1 = 'Higher-Tenor', 'Higher-Tenor'
+    TENOR2 = 'Lower-Tenor', 'Lower-Tenor'
+    BARITONE = 'Baritone', 'Baritone'
+    BASSO = 'Basso', 'Basso'
+    CONTRABASSO = 'Contra-Basso', 'Contra-Basso'
 
 class SkillLevel(models.TextChoices):
     BEGINNER = "Beginner", "Beginner"
@@ -37,24 +40,39 @@ class SkillLevel(models.TextChoices):
     SCHOOLED = "Schooled", "Schooled"
     PROFESSIONAL = "Professional", "Professional"
 
+class Attendance(models.TextChoices):
+    PRESENT = "Present", "Present"
+    ABSENT = "Absent", "Absent"
+    #LATE = "Late", "Late"
+
+
+
 class Attendee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    instrument = models.CharField(max_length=2, choices=[(voice.name, voice.value) for voice in VoiceType])
+    instrument = models.CharField(max_length=12, choices=[(voice.name, voice.value) for voice in VoiceType])
     is_active = models.BooleanField(default=True)
-    skill_level = models.CharField(max_length=20, choices=SkillLevel.choices,default=SkillLevel.ENTHUSIAST)
+    skill_level = models.CharField(max_length=20, choices=[(skill.name, skill.value) for skill in SkillLevel])
     phone_number = models.CharField(max_length=17, blank=True, null=True)
     email = models.EmailField(unique=True, blank=True, null=True)
     address = models.CharField(max_length=250, blank=True, null=True)
     date_of_birth = models.DateField("birthday")
-    messenger = models.CharField(max_length=250, blank=True)
+    messenger = models.CharField(max_length=250, blank=True, null=True)
     #photo =    ->    # to be added later
     shirt_size = models.CharField(max_length=4, choices=[(size.name, size.value) for size in ShirtSize])
     date_of_joining = models.DateField("joined",default=date.today)
     additional_attendee_notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} {self.get_instrument_display()}"
+        return f"{self.first_name} {self.last_name} {self.instrument}" #get_instrument_display()}"
+
+class AttendanceRecord(models.Model):
+    attendee = models.ForeignKey(Attendee, on_delete=models.PROTECT,related_name="attendance_records")
+    attendance = models.CharField(choices=[(status.name, status.value) for status in Attendance])
+
+
+
+
 
 
 
@@ -79,14 +97,6 @@ class RehearsalDate(models.Model):
     #    return f"{self.rehearsal_subtitle}"
 
 class Song(models.Model):
-    # choices within Songs class:
-    #DIFFICULTY_LEVEL = [
-    #    ("1", "Beginner"),
-    #    ("2", "Enthusiast"),
-    #    ("3", "Experienced"),
-    #    ("4", "Schooled"),
-    #    ("5", "Professional"),
-    #]
     song_title =  models.CharField(max_length=250)
     song_composer =  models.CharField(max_length=250)
     song_author = models.CharField(max_length=250)
@@ -96,12 +106,12 @@ class Song(models.Model):
     number_of_voices = models.IntegerField(validators=[MinValueValidator(1)])
     song_difficulty_level =  models.CharField(max_length=20, choices=SkillLevel.choices, default=SkillLevel.EXPERIENCED)
     transcript = models.TextField()
-    translation = models.TextField(blank=True)
-    speech_articulation = models.TextField(blank=True)
-    # date_of_rehearsal = models.DateField("date of rehearsal")
-    date_of_concert = models.ForeignKey(RehearsalDate, on_delete=models.PROTECT, related_name='song', blank=True, null=True) #, default=1 #
+    translation = models.TextField(blank=True, null=True)
+    speech_articulation = models.TextField(blank=True, null=True)
+    #date_of_rehearsal = models.DateField("date of rehearsal")  # -> for later implementation
+    #date_of_concert = models.ForeignKey(RehearsalDate, on_delete=models.PROTECT, related_name='song', blank=True, null=True) #, default=1 # -> for later implementation
     date_added = models.DateField("added to archive", default=date.today)
-    additional_songs_notes = models.TextField(blank=True)
+    additional_songs_notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.song_title} {self.song_composer}"
@@ -109,27 +119,23 @@ class Song(models.Model):
 
 
 class CurrentRehearsal(models.Model):
-    class Attendance(models.TextChoices):
-        Present = "present", "present"
-        Absent = "absent", "absent"
-        Late = "late", "late"
 
     rehearsal = models.ForeignKey(RehearsalDate, on_delete=models.CASCADE, related_name='current_rehearsals', default=1)  # Default to the default_rehearsal
-    attendance = models.CharField(max_length=9, choices=Attendance.choices)
-    song = models.ForeignKey(Song, on_delete=models.PROTECT, related_name='current_rehearsals')
-    attendee = models.ForeignKey(Attendee, on_delete=models.PROTECT, related_name='current_rehearsals')
+    #attendance = models.CharField(max_length=9, choices=Attendance.choices)    # temporary out!
+    #song = models.ForeignKey(Song, on_delete=models.PROTECT, related_name='current_rehearsals')
+    #attendee = models.ForeignKey(Attendee, on_delete=models.PROTECT, related_name='current_rehearsals')    # temporary out!
 
-    def save(self, *args, **kwargs):
-        if not self.rehearsal_id:
-            default_rehearsal = RehearsalDate.objects.create(
-                rehearsal_subtitle = "DefaultRehearsal",
-                rehearsal_location = "Usual Location",
-                rehearsal_location_parking = "",
-                rehearsal_calendar = timezone.now(),
-                additional_rehearsal_notes = "This is the default rehearsal"
-            )
-            self.rehearsal = default_rehearsal
-        super().save()
+    #def save(self, *args, **kwargs):
+    #    if not self.rehearsal_id:
+    #        default_rehearsal = RehearsalDate.objects.create(
+    #            rehearsal_subtitle = "DefaultRehearsal",
+    #            rehearsal_location = "Usual Location",
+    #            rehearsal_location_parking = "",
+    #            rehearsal_calendar = timezone.now(),
+    #            additional_rehearsal_notes = "This is the default rehearsal"
+    #        )
+    #        self.rehearsal = default_rehearsal
+    #    super().save()
 
     def __str__(self):
-        return f"{self.rehearsal} {self.attendee} {self.get_attendance_display()}"
+        return f"{self.rehearsal}" #{self.attendee} {self.get_attendance_display()}"  # —> for a test I comment this out
