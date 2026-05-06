e6ed9dbfcb208631d4c432875ce8ecbffff415db
lukaklanjsek <luka.klanjsek@gmail.com>
2026-05-06 13:33:56 +0200

    cleanup for publishing

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # cleanup
-rm secondapp/migrations/0*.py
+rm syncope/migrations/0*.py
 rm db.sqlite3
 
 # setup new db
 uv run manage.py makemigrations
 uv run manage.py migrate
-uv run manage.py loaddata secondapp/fixtures/secondapp/*.json
+uv run manage.py loaddata syncope/fixtures/syncope/*.json
 
 # start server
 uv run manage.py runserver
